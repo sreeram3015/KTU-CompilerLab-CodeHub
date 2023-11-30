a5991d7168511de372db36e6227aab21b669ca54
Sreeram P <psreeram3015@proton.me>
2023-11-30 19:59:16 +0530

    Renamed variable names so that it would be better understandable.

--- 1 - Lexical Analyzer Using C/lexicalAnalyzerUsingC.c
+++ 1 - Lexical Analyzer Using C/lexicalAnalyzerUsingC.c
@@ -4,90 +4,90 @@
 
 int main() {
     // Declare file pointers for input and output
-    FILE *input, *output;
+    FILE *inputFile, *outputFile;
 
     // Initialize line number, token number, and flags
-    int l = 1;
-    int t = 0;
-    int j = 0;
-    int i, flag;
+    int lineNumber = 1;
+    int tokenNumber = 0;
+    int keywordIndex = 0;
+    int charIndex, isKeyword;
 
     // Declare character and string variables
-    char ch, str[20];
+    char currentChar, lexeme[20];
 
     // Open input file in read mode and output file in write mode
-    input = fopen("input.txt", "r");
-    output = fopen("output.txt", "w");
+    inputFile = fopen("input.txt", "r");
+    outputFile = fopen("output.txt", "w");
 
     // Declare an array of keywords
-    char keyword[30][30] = {"int", "main", "if", "else", "do", "while"};
+    char keywords[30][30] = {"int", "main", "if", "else", "do", "while"};
 
     // Print header in the output file
-    fprintf(output, "Line no. \t Token no. \t Token \tLexeme\n\n");
+    fprintf(outputFile, "Line no. \t Token no. \t Token \tLexeme\n\n");
 
     // Loop until the end of the input file is reached
-    while (!feof(input)) {
-        i = 0;  // Initialize index for the string
-        flag = 0;  // Reset the flag
-        ch = fgetc(input);  // Read a character from the input file
+    while (!feof(inputFile)) {
+        charIndex = 0;  // Initialize index for the lexeme
+        isKeyword = 0;  // Reset the flag
+        currentChar = fgetc(inputFile);  // Read a character from the input file
 
         // Check if the character is an operator (+, -, *, /)
-        if (ch == '+' || ch == '-' || ch == '*' || ch == '/') {
-            fprintf(output, "%7d\t\t %7d\t\t Operator\t %7c\n", l, t, ch);
-            t++;
+        if (currentChar == '+' || currentChar == '-' || currentChar == '*' || currentChar == '/') {
+            fprintf(outputFile, "%7d\t\t %7d\t\t Operator\t %7c\n", lineNumber, tokenNumber, currentChar);
+            tokenNumber++;
         }
         // Check if the character is a special symbol (;, {, }, (, ), ?, @, !, %)
-        else if (ch == ';' || ch == '{' || ch == '}' || ch == '(' || ch == ')' || ch == '?' || ch == '@' || ch == '!' || ch == '%') {
-            fprintf(output, "%7d\t\t %7d\t\t Special symbol\t%7c\n", l, t, ch);
-            t++;
+        else if (currentChar == ';' || currentChar == '{' || currentChar == '}' || currentChar == '(' || currentChar == ')' || currentChar == '?' || currentChar == '@' || currentChar == '!' || currentChar == '%') {
+            fprintf(outputFile, "%7d\t\t %7d\t\t Special symbol\t%7c\n", lineNumber, tokenNumber, currentChar);
+            tokenNumber++;
         }
         // Check if the character is a digit
-        else if (isdigit(ch)) {
-            fprintf(output, "%7d\t\t %7d\t\t Digit\t\t %7c\n", l, t, ch);
-            t++;
+        else if (isdigit(currentChar)) {
+            fprintf(outputFile, "%7d\t\t %7d\t\t Digit\t\t %7c\n", lineNumber, tokenNumber, currentChar);
+            tokenNumber++;
         }
         // Check if the character is an alphabet
-        else if (isalpha(ch)) {
-            str[i] = ch;
-            i++;
+        else if (isalpha(currentChar)) {
+            lexeme[charIndex] = currentChar;
+            charIndex++;
 
             // Read subsequent characters forming an identifier or keyword
-            ch = fgetc(input);
-            while (isalnum(ch) && ch != ' ') {
-                str[i] = ch;
-                i++;
-                ch = fgetc(input);
+            currentChar = fgetc(inputFile);
+            while (isalnum(currentChar) && currentChar != ' ') {
+                lexeme[charIndex] = currentChar;
+                charIndex++;
+                currentChar = fgetc(inputFile);
             }
 
             // Terminate the string
-            str[i] = '\0';
+            lexeme[charIndex] = '\0';
 
             // Check if the string is a keyword
-            for (j = 0; j < 6; j++) {
-                if (strcmp(str, keyword[j]) == 0) {
-                    flag = 1;
+            for (keywordIndex = 0; keywordIndex < 6; keywordIndex++) {
+                if (strcmp(lexeme, keywords[keywordIndex]) == 0) {
+                    isKeyword = 1;
                     break;
                 }
             }
 
             // Print either keyword or identifier based on the flag
-            if (flag == 1) {
-                fprintf(output, "%7d\t\t %7d\t\t Keyword\t%7s\n", l, t, str);
-                t++;
+            if (isKeyword) {
+                fprintf(outputFile, "%7d\t\t %7d\t\t Keyword\t%7s\n", lineNumber, tokenNumber, lexeme);
+                tokenNumber++;
             } else {
-                fprintf(output, "%7d\t\t %7d\t\t Identifier\t%7s\n", l, t, str);
-                t++;
+                fprintf(outputFile, "%7d\t\t %7d\t\t Identifier\t%7s\n", lineNumber, tokenNumber, lexeme);
+                tokenNumber++;
             }
         }
         // Check if the character is a newline character
-        else if (ch == '\n') {
-            l++;  // Increment the line number
+        else if (currentChar == '\n') {
+            lineNumber++;  // Increment the line number
         }
     }
 
     // Close the input and output files
-    fclose(input);
-    fclose(output);
+    fclose(inputFile);
+    fclose(outputFile);
 
     // Return 0 to indicate successful execution
     return 0;
